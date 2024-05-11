4929f283167b79fd15fbcf2339be34d17b8c3f21
Mohmed Albakro <mohamadalbakro104@gmial.com>
2024-05-11 15:28:07 +0300

    dddssl

--- database/migrations/2024_04_29_101034_create_auctions_table.php
+++ database/migrations/2024_04_29_101034_create_auctions_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->date('end');
             $table->date('begin');
+            $table->time('limit');
             $table->string('description');
             $table->integer('initialPrice');
             $table->unsignedBigInteger('profile_id');
